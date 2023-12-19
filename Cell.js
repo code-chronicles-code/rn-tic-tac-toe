dda8da0ad494c8d1bb6b49494961d5ca97f0a6e6
Miorel-Lucian Palii <mlpalii@gmail.com>
2023-12-18 18:43:10 -0800

    Use SVG to render the X and O symbols

--- Cell.js
+++ Cell.js
@@ -1,11 +1,46 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import Svg, { Circle, Line } from 'react-native-svg';
+
+const SIZE = 80;
 
 function X() {
-  return <Text style={[styles.text, { color: 'navy' }]}>X</Text>;
+  return (
+    <Svg height={SIZE} width={SIZE} viewBox="0 0 100 100">
+      <Line
+        x1="20"
+        y1="20"
+        x2="80"
+        y2="80"
+        stroke="navy"
+        strokeWidth="8"
+        strokeLinecap="round"
+      />
+      <Line
+        x1="80"
+        y1="20"
+        x2="20"
+        y2="80"
+        stroke="navy"
+        strokeWidth="8"
+        strokeLinecap="round"
+      />
+    </Svg>
+  );
 }
 
 function O() {
-  return <Text style={[styles.text, { color: 'crimson' }]}>O</Text>;
+  return (
+    <Svg height={SIZE} width={SIZE} viewBox="0 0 100 100">
+      <Circle
+        cx="50"
+        cy="50"
+        r="35"
+        stroke="crimson"
+        strokeWidth="8"
+        fill="transparent"
+      />
+    </Svg>
+  );
 }
 
 export default function Cell({ cellValue, onPress }) {
@@ -21,7 +56,7 @@ export default function Cell({ cellValue, onPress }) {
         return null;
     }
 
-    console.error('Unexpected cell value!');
+    console.error('Unexpected cell value!', cellValue);
     return null;
   })();
 
@@ -34,14 +69,11 @@ export default function Cell({ cellValue, onPress }) {
 
 const styles = StyleSheet.create({
   container: {
-    width: 80,
+    width: SIZE,
     aspectRatio: 1,
     borderRadius: 8,
     backgroundColor: 'lightgray',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  text: {
-    fontSize: 60,
   },
 });
